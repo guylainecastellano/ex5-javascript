d59592f7557c10f21c0dc63dd61700ff256ff4c5
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-04-30 14:17:59 +0200

    ex5 javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//var + alert
+//ou fonction ==> récuperer les nombre et faire le calcul de la division 5/2 =2.5 doit s'afficher 1
+// trouver le nom  --le reste de la division trouver le nom --modulo
+function clickFunction(){
+  var number1 = document.getElementById('number1').value;
+  var number2 = document.getElementById('number2').value;
+  var result = number1 % number2;
+  alert(result);
+}
